736d4ff8622051496540c5a526270ed11caf815f
LEONARDO GRAMMALDO <l.grammaldo@nh-hotels.com>
2020-10-27 07:35:24 -0300

    se soluciona problema de acceso a carpetas al correr express, se asignan variables publicas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
 const PORT = 3000;
+const app = express();
 
+app.use(express.static(__dirname + "/public"))
 app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log("servidor corriendo " + PORT);
 });
